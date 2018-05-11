32826ace3cbbf04fd2e6f1e9b3bea5444c2ab8d2
Brendan <BCleveland@student.neumont.edu>
2018-05-11 10:21:43 -0600

    Player/Human/Computer stubbed out

---
+++ GameOfNim/Player.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfNim
+{
+	public abstract class Player
+	{
+		public string playerName;
+		public abstract void TakeTurn(out int Heap, out int Amount);
+	}
+}
