cd6d4c9245658203bb19c77f84f8bb76093bf31c
Brendan <BCleveland@student.neumont.edu>
2018-05-11 10:52:04 -0600

    Easy and Medium AI Difficulties potentially functional

--- GameOfNim/Computer.cs
+++ GameOfNim/Computer.cs
@@ -31,13 +31,53 @@ namespace GameOfNim
 		}
 		private void TurnEasy(out int Heap, out int Amount)
 		{
-			Heap = 0;
-			Amount = 0;
+			int[] board = NimController.Instance.GetHeaps();
+			Random rng = new Random();
+			Heap = rng.Next(board.Length);
+			while(board[Heap] != 0)
+			{
+				Heap = rng.Next(board.Length);
+			}
+			if(board[Heap] == 1)
+			{
+				Amount = 1;
+				return;
+			}
+			Amount = rng.Next(1, board[Heap]-1);
 		}
 		private void TurnMedium(out int Heap, out int Amount)
 		{
-			Heap = 0;
-			Amount = 0;
+			int[] board = NimController.Instance.GetHeaps();
+			List<int> preferedIndicies = new List<int>();
+			for(int j = 0; j < board.Length; j++)
+			{
+				if (board[j] >= 2) preferedIndicies.Add(j);
+			}
+			if (preferedIndicies.Count == 0)
+			{
+				//No piles with 2 remaing
+				for (int j = 0; j < board.Length; j++)
+				{
+					if (board[j] == 1)
+					{
+						Heap = j;
+						Amount = 1;
+						return;
+					}
+				}
+				throw new ArgumentException("Error: An empty board was passed to TurnMedium.");
+			}
+			else if (preferedIndicies.Count == 1)
+			{
+				Heap = preferedIndicies[0];
+				Amount = board[Heap] - 1;
+			}
+			else
+			{
+				Random rng = new Random();
+				Heap = preferedIndicies[rng.Next(preferedIndicies.Count)];
+				Amount = 2;
+			}
 		}
 		private void TurnHard(out int Heap, out int Amount)
 		{
