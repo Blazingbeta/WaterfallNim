56bc27e5cb2b1e61b063be8483400b7f47f0c05e
JoshuaKey <keyjoshj98@yahoo.com>
2018-05-11 10:31:53 -0600

    Added Input Functions

--- GameOfNim/NimController.cs
+++ GameOfNim/NimController.cs
@@ -7,10 +7,10 @@ using System.Threading.Tasks;
 namespace GameOfNim {
     public class NimController {
 
-        //Player player1;
-        //Player player2;
-        //AudioManager audioMan;
-        int[] heaps;
+        //private Player player1;
+        //private Player player2;
+        //private AudioManager audioMan;
+        private int[] heaps;
         public static NimController Instance;
 
         public void SetUpInstance() {
@@ -22,17 +22,43 @@ namespace GameOfNim {
         }
 
         public void DisplayHeaps() {
-
+            for(int i = 0; i < heaps.Length; i++) {
+                for(int y = 0; y < heaps[i]; y++) {
+                    Console.Write("| ");
+                }
+                Console.WriteLine();
+            }
         }
 
         public static string GetStringInput() {
-            return "";
+            string input = Console.ReadLine();
+
+            return input;
         }
         public static int GetIntInput() {
-            return 0;
+            int num = 0;
+
+            while(!int.TryParse(Console.ReadLine(), out num)) {
+                Console.WriteLine("That is not a valid int.");
+            }
+
+            return num;
         }
         public static int GetMenuInput(List<string> options) {
-            return 0;
+            bool isInvalid = true;
+            int num = 0;
+            while (isInvalid) {
+                for (int i = 0; i < options.Count; i++) {
+                    Console.WriteLine(i + 1 + ". " + options[i]);
+                }
+                num = GetIntInput();
+                isInvalid = num < 1 || num > options.Count;
+                if (isInvalid) {
+                    Console.WriteLine("Please enter a number between 1 and " + options.Count);
+                }
+            }
+
+            return num;
         }
 
         public int[] GetHeaps() {
