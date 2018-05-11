1ca1369cae70e2dd66232777722c0afe3c35b45f
JoshuaKey <keyjoshj98@yahoo.com>
2018-05-11 10:35:52 -0600

    Stubbed out AuidioManager

---
+++ GameOfNim/AudioManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfNim {
+    public class AudioManager {
+
+        public void ValidBeep() {
+
+        }
+
+        public void InvalidBeep() {
+
+        }
+
+        public bool StartMusic() {
+            return false;
+        }
+
+    }
+}
