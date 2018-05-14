55e5487f2d2f705a63a5ec261d4f29302d89e158
Evan Lamb <evanl1719@gmail.com>
2018-05-13 22:28:47 -0600

    Fleshed out Human, found bugs.
    Game does not end when a player wins or loses.

--- GameOfNim/Human.cs
+++ GameOfNim/Human.cs
@@ -10,12 +10,56 @@ namespace GameOfNim
 	{
 		public Human(string name, int playerNum)
 		{
-			playerName = name;
+            if(name == null || name == "")
+            {
+                playerName = "Player " + playerNum;
+            }
+            else
+            {
+			    playerName = name;
+            }
+
 			//If name is invalid, name is "Player " + playerNum
 		}
 		public override void TakeTurn(out int Heap, out int Amount)
 		{
-			throw new NotImplementedException();
+            int[] board = NimController.Instance.GetHeaps();
+
+            int tempHeap = 0;
+            int tempAmount = 0;
+
+            while(true)
+            {
+                Console.WriteLine("Please enter a heap number between 0 and " + (board.Length - 1));
+                tempHeap = NimController.GetIntInput();
+                if (tempHeap > board.Length - 1 ||tempHeap < 0)
+                {
+                    Console.WriteLine("That is not a valid heap selection.");
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            while(true)
+            {
+                Console.WriteLine("Please enter the number you want to grab, (Up to " + board[tempHeap] + ")");
+                tempAmount = NimController.GetIntInput();
+                if(tempAmount > board[tempHeap] || tempAmount < 1)
+                {
+                    Console.WriteLine("That is not a valid amount to select.");
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+
+            Heap = tempHeap;
+            Amount = tempAmount;
+
 		}
 	}
 }
